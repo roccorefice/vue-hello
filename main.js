45a182ae41e133bdb5186cbb0269b83a044c62e7
Rocco Orefice <rocco.orefice92@gmail.com>
2020-12-14 15:50:04 +0100

    primo commit
    Stampo a schermo un h1 e img con data-vuejs

---
+++ main.js
@@ -0,0 +1,9 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+let app = new Vue ({
+    el: "#root",
+    data : {
+        message: "sono un h1 stampato attraverso VueJs",
+        image: "./learningvue.jpg"
+    }
+});
